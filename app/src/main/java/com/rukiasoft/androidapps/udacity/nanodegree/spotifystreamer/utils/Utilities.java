5cce35d3d63504213494ec66858d34bd7669fb77
Raúl Feliz <raulfeliz@gmail.com>
2015-07-08 18:52:13 +0200

    translate strings

--- app/src/main/java/com/rukiasoft/androidapps/udacity/nanodegree/spotifystreamer/utils/Utilities.java
+++ app/src/main/java/com/rukiasoft/androidapps/udacity/nanodegree/spotifystreamer/utils/Utilities.java
@@ -15,6 +15,7 @@ import com.rukiasoft.androidapps.udacity.nanodegree.spotifystreamer.ToolbarAndRe
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
